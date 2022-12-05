623a18ff63f8397c2cc8f723d0f088558c626509
phidinz <divinerestu0@gmail.com>
2022-12-05 10:08:52 +0700

    zakat view bug

--- resources/views/FrontEnd/zakat/index.blade.php
+++ resources/views/FrontEnd/zakat/index.blade.php
@@ -9,44 +9,29 @@
     <div class="container">
         <form action="" method="post">
             @csrf
-            <!-- <div class="form-group">
+           
+            <div class="form-group">
                 <label for="kategori_zakat">{{ __('Kategori Zakat') }}</label>
                 <select name="kategori_zakat" id="id_zakat" class="form-control">
                     @foreach($kalkulator as $item)
-                    <option value="{{ $item->kategori_zakat}}">{{ $item->kategori_zakat }}</option>
+                    <option value="{{ $item->kategori_zakat }}">{{ $item->kategori_zakat }}</option>
                     @endforeach
+            
                 </select>
-            </div> -->
-            <div class="form-group">
-                <div class="row">
-                    <div class="col">
-                        <label for="kategori_zakat">{{ __('kategori zakat') }}</label>
-                        <select name="kategori_zakat" class="form-control">
-                            <option value="Zakat Fitrah">{{__('Zakat Fitrah')}}</option>
-                            <option value="Zakat Penghasilan">{{__('Zakat Penghasilan')}}</option>
-                        </select>
-                    </div>
-
-                </div>
             </div>
-            @if($item->id_zakat == 'ZK01')
+          
+            @if($item->id_zakat == 'ZK02')
                 <div class="row">
                     <div class="col">
                         <label for="nik">{{ __('Harga Beras') }}</label>
                         <input type="text" class="form-control" name="nik" required>
                     </div>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="row">
                     <div class="col">
                         <label for="nik">{{ __('Jumlah Zakat') }}</label>
                         <input type="text" class="form-control" name="nik" required>
                     </div>
                 </div>
-            </div>
-            @else
+            @elseif($item->id_zakat)
             <div class="form-group">
                 <div class="row">
                     <div class="col">
