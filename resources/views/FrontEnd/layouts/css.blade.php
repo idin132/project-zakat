a0bbc17023b7ffdf8f9fe0f748843527aca8ece8
phidinz <divinerestu0@gmail.com>
2022-12-15 15:28:15 +0700

    add slick slider

--- resources/views/FrontEnd/layouts/css.blade.php
+++ resources/views/FrontEnd/layouts/css.blade.php
@@ -12,4 +12,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.css') }}">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
     <link rel="stylesheet" href="{{asset('css/frontend.css')}}">
