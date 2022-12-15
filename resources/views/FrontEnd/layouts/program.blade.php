8be16b6e11b36020e4e721b1e44e8286d0852308
phidinz <divinerestu0@gmail.com>
2022-12-15 15:59:09 +0700

    add program title

--- resources/views/FrontEnd/layouts/program.blade.php
+++ resources/views/FrontEnd/layouts/program.blade.php
@@ -1,5 +1,10 @@
 <div class="container" style="background-color: #e8ecf2; padding:25px;">
-    
+    <center>
+        <div class="badge" style="background-color: #65ffdc;">
+            <h6>Program</h6>
+        </div>
+    </center>
+    <br>
     <div class="program">
         <div class="card" style="width: 18rem;">
             <img class="card-img-top" src="{{asset('images/slider_zakat_fitrah.jpeg')}}">
