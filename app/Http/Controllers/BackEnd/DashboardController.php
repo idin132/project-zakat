b142d88f92788b2889060047ab50661ae3ccd602
phidinz <divinerestu0@gmail.com>
2023-02-13 15:19:03 +0700

    add procedure from database

--- app/Http/Controllers/BackEnd/DashboardController.php
+++ app/Http/Controllers/BackEnd/DashboardController.php
@@ -8,19 +8,21 @@ use App\Models\User;
 use App\Models\mustahiq;
 use App\Models\pembayaran;
 use App\Models\penerimaan;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        $mustahiqs = mustahiq::count();
-        $muzakkis = User::count();
-        $penerimaan = penerimaan::count();
-        $pembayaran = pembayaran::count();
-        // $total_donasi = pembayaran::sum('jumlah');
-        $total_donasi_disetujui = pembayaran::where('status', '0')->sum('jumlah');
+        $mustahiqs = mustahiq::count(); // total mustahiq //
+        $muzakkis = User::count(); // total user/muzakki //
+        $penerimaan = penerimaan::count(); // total penerimaan ke mustahiq //
+        $pembayaran = pembayaran::count(); // total pembayaran dari muzakki //
+        $total_donasi = pembayaran::sum('jumlah'); 
+        // $total_donasi_disetujui = pembayaran::where('status', '0')->sum('jumlah');
+        $results = DB::table('pembayaran')->select(DB::raw("CALL JumlahZakat()"))->where('status', '0')->sum('jumlah'); // Procedure //
         $total_tersalurkan = penerimaan::sum('jumlah');
-        return view('BackEnd.dashboard', compact('mustahiqs','muzakkis', 'penerimaan', 'pembayaran','total_donasi_disetujui', 'total_tersalurkan'));
+        return view('BackEnd.dashboard', compact('mustahiqs','results','muzakkis', 'penerimaan', 'pembayaran', 'total_tersalurkan'));
     }
 
 }
