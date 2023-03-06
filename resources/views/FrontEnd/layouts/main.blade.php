f749600172916cbf48a07a4e6995a18b3bc2fc40
phidinz <divinerestu0@gmail.com>
2023-03-06 21:24:46 +0700

    add preloader

--- resources/views/FrontEnd/layouts/main.blade.php
+++ resources/views/FrontEnd/layouts/main.blade.php
@@ -7,8 +7,17 @@
 </head>
 
 <body>
+<div class="loader">
+        <div></div>
+    </div>
+
+    <div class="content">
     @include('FrontEnd.layouts.nav')
     @yield('content')
+
+    @include('FrontEnd.layouts.footer')
+    @yield('footer')
+    </div>
     @include('FrontEnd.layouts.js')
     @yield('js')
     @include('FrontEnd.layouts.footer')
