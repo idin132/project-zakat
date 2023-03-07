df383878f7d7796cac91d1999682c063c78cdb30
phidinz <divinerestu0@gmail.com>
2023-03-07 07:25:21 +0700

    Add preloader

--- resources/views/FrontEnd/layouts/js.blade.php
+++ resources/views/FrontEnd/layouts/js.blade.php
@@ -1,10 +1,10 @@
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.js"></script>
     <script src="{{ asset('assets/js/owl.carousel.js') }}"></script>
-    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script>
         $('.owl-carousel').owlCarousel({
             loop: true,
@@ -29,5 +29,10 @@
                 infinite: true,
                 slidesToShow: 3,
             });
+
+        });
+        $(window).on('load',function(){
+            $(".loader").fadeOut(4000);
+            $(".content").fadeIn(4000);
         });
     </script>
